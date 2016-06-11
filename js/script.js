24c497a25fb7b55a39c4cc3be1c51bbceda4959a
FuYJ <fuyiru@fuyirudeMacBook-Pro.local>
2016-06-11 16:18:09 +0800

    hue testing

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(function(){
+  var $thumbs = $('.animate1');
+  var $nav = $('.navbar-fixed-top');
+  var winheight = $(window).height();
+  var fullheight = $(document).height();
+
+});
+
+
+new WOW().init();
