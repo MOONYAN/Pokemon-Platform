7233f61ff0e72a217fa75b2cf8b9d5ba5fa5e14b
FuYJ <zz28234830@gmail.com>
2016-06-20 21:06:10 +0800

    edit logo & modify IntroducePet page

--- js/IntroducePet.js
+++ js/IntroducePet.js
@@ -8,16 +8,17 @@ function Start() {
     petId = Request.parameter('petId');
     if (!petId)
     {
-        //$('#_petContent').addClass('hide');
+        $('#_petContent').addClass('hide');
         console.log('none petId');
     }
     else
     {
-        $('.fb-like').data('href', "https://owen-pokemon.herokuapp.com/IntroducePet.html?petId=" + petId);
-        $('.fb-comments').data('href', "https://owen-pokemon.herokuapp.com/IntroducePet.html?petId=" + petId);
+        $('.fb-like').data('href', "https://owen-pokemon.herokuapp.com/?petId=" + petId);
+        $('.fb-comments').data('href', "https://owen-pokemon.herokuapp.com/?petId=" + petId);
         console.log(petId);
         RetrieveData();
     }
+    LoginSuccess();
 }
 
 function RetrieveData() {
@@ -33,18 +34,11 @@ function RetrieveData() {
         var _imageURL = "url(" + petData.ImageURL + ")";
         $('#_imageURLImage').css("background-image", _imageURL);
         //$('#_youtubeURLText').val(petData.YoutubeURL);
-        //player.cueVideoById(petData.YoutubeURL);
-
+        player.cueVideoById(petData.YoutubeURL);
         $('#_cellPhoneURLText').val(petData.CellPhone);
-
-        player = new YT.Player('player', {
-            height: '390',
-            width: '640',
-            videoId: petData.YoutubeURL
-        });
     });
 }
-/*
+
 function onYouTubeIframeAPIReady()
 {
     player = new YT.Player('player', {
@@ -53,4 +47,21 @@ function onYouTubeIframeAPIReady()
         //videoId: petData.YoutubeURL
     });
 }
-*/
+
+function LoginSuccess() {
+    //localStorage.setItem('user', user);
+    //localStorage.setItem("loginState", "login");
+    //sessionStorage["loginState"] = "login";
+
+    //$('#_displayNameLabel').text(user.displayName);
+    //$('#_emailLabel').text(user.email);
+    //$('#_photoURLLabel').text(user.photoURL);
+    $('#_photoURLImage').attr("src", user.photoURL);
+
+    //$('#_uidLabel').text(user.uid);
+    //$('#_loginButton').addClass('hide');
+    //$('._loginAnchor').addClass('hidden');
+    $('._loginAnchor a').text('Logout');
+    //$('._logoutAnchor').removeClass('hidden');
+    //$('#_hidden').val(user.providerId);
+}
