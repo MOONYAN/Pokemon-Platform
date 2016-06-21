49af7c4e509c315adaa7ba5e246f97eab04522d0
FuYJ <zz28234830@gmail.com>
2016-06-21 13:40:55 +0800

    test font2

--- js/Pets_Dog.js
+++ js/Pets_Dog.js
@@ -99,6 +99,8 @@ function CheckAdmin()
         $('#_photoURLImage').attr("src", user.photoURL);
         $('._loginAnchor a').text('Logout');
         isAdmin = user.admin;
+    } else {
+        LoginFail();
     }
     console.log(isAdmin);
 }
@@ -168,4 +170,9 @@ function RetrieveData()
             CheckPictureHeight();
         });
     });
+}
+
+function LoginFail() {
+    $('._user').addClass("hide");
+    $('#_photoURLImage').addClass("hide");
 }
